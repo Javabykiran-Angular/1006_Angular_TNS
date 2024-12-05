1fba00732d6ad023485faefa3c0a2be6af83fcb8
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2024-12-05 18:18:10 +0530

    Day07

---
+++ Typescript/Day07/demo2.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var demo1_1 = require("./demo1");
+var obj = new demo1_1.Myclass(6, 'Kiran', 'Raokhande');
+obj.display();
+console.log("\n        Addition :: ".concat((0, demo1_1.add)(10, 20), "\n    "));
+console.log("Pi is " + demo1_1.pi);
---
+++ Typescript/Day07/demo2.ts
@@ -0,0 +1,11 @@
+import {Myclass,add,pi} from './demo1'
+
+let obj=new Myclass(6,'Kiran','Raokhande');
+obj.display();
+
+
+console.log(`
+        Addition :: ${add(10,20)}
+    `);
+
+console.log("Pi is "+pi);
