493f9c9b4a30f9f6dcdc177f9d33367b519e2030
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2024-12-13 15:16:25 +0530

    Day09

---
+++ Angular/Day09/post/post.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit {
+
+  posts:any[]=[];
+
+  constructor(private service:HttpService){}
+  ngOnInit(): void {
+    this.getDataFromBackend();
+  }
+
+  getDataFromBackend(){
+      this.service.getPost()
+      .subscribe((response:any)=>{
+        // console.log(response);
+        this.posts=response;
+      })
+  }
+
+  onSubmit(mytitle:any,mybody:any){
+    let jsonObj={
+      tilte:mytitle,
+      body:mybody
+    }
+
+    this.service.postData(jsonObj)
+    .subscribe((response)=>{
+      console.log(response);
+      
+    })
+
+  }
+
+
+
+}
