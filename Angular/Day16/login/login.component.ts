13a0ab6b6c592fa384e6fd51c5659ae5ddf76d38
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2024-12-18 11:53:43 +0530

    Update

--- Angular/Day16/login/login.component.ts
+++ Angular/Day16/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-login',
@@ -12,11 +13,20 @@ export class LoginComponent implements OnInit {
   EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
   + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 
+  constructor(private service:HttpService){}
+
+
   ngOnInit(): void {
     document.body.className='bg_background';
   }
 
   onSubmit(f:NgForm){
+    let obj={
+      emailid:f.value.emailid,
+      password:f.value.password
+    }
+
+    
 
   }
 
